831386e47cb1d60da456f2e0ee21338a1e19b1ba
oathiago-tech <oathiago@icloud.com>
2025-10-26 22:07:39 -0300

    First commit

---
+++ src/main/java/com/tao/sbootbalanceservice/SbootBalanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tao.sbootbalanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootBalanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootBalanceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tao/sbootbalanceservice/SbootBalanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tao.sbootbalanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootBalanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
